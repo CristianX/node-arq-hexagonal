21a17ccfc12724ea707c411c14a85e16f38fd06d
CristianX <thecristianx@hotmail.com>
2023-01-03 21:07:13 -0500

    Modularizando Express y añadiendo archivo de configuración

--- server/index.js
+++ server/index.js
@@ -1,13 +1,9 @@
 const express = require('express');
 
-const port = 3000;
-
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hola Mundo!');
 });
 
-app.listen(port, () => {
-  console.log(`App de ejemplo corriendo en http://localhost:${port}`);
-});
+module.exports = app;
