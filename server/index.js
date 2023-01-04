6d4e1df57e50ced346f9004fd705b124401726b2
CristianX <thecristianx@hotmail.com>
2023-01-03 22:37:12 -0500

    Anadiendo middleware para rutas no encontradas y errores

--- server/index.js
+++ server/index.js
@@ -2,8 +2,28 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hola Mundo!');
+app.get('/', (req, res, next) => {
+  res.json({
+    message: 'Bienvenido a la API LMU40',
+  });
+});
+
+// Manejo de ruta no encontrada
+app.use((req, res, next) => {
+  res.status(404);
+  res.json({
+    message: 'ERROR: Ruta no encontrada',
+  });
+});
+
+// Manejo de errores
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res.status(statusCode);
+  res.json({
+    message,
+  });
 });
 
 module.exports = app;
