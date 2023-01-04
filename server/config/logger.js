94b853cda4701dd8f6d501c82a3801229ad3a792
CristianX <thecristianx@hotmail.com>
2023-01-03 23:27:14 -0500

    Añadiendo request id para identificar las peticiones con morgan

--- server/config/logger.js
+++ server/config/logger.js
@@ -1,9 +1,27 @@
 const { createLogger, format, transports } = require('winston');
+const morgan = require('morgan');
+const stripFinalNewline = require('strip-final-newline');
 
 // Configurando Logger
 const logger = createLogger({
   format: format.simple(),
-  transports: [transports.Console()],
+  transports: [new transports.Console()],
 });
+
+// Configurando request logger
+morgan.token('id', (req) => req.id);
+
+const requestFormat = ':remote-addr[:date[iso]]:id":method:url":status';
+const requests = morgan(requestFormat, {
+  stream: {
+    write: (message) => {
+      const log = stripFinalNewline(message);
+      return logger.info(log);
+    },
+  },
+});
+
+// Adjuntando al logger object
+logger.requests = requests;
 
 module.exports = logger;
--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const morgan = require('morgan');
+const requestId = require('express-request-id')();
 
 const logger = require('./config/logger');
 
+// iniciando app
 const app = express();
 
 // configurando Middleware
-app.use(
-  morgan('combined', { stream: { write: (message) => logger.info(message)}})
-);
+app.use(requestId);
+app.use(logger.requests);
 
 app.get('/', (req, res, next) => {
   res.json({
