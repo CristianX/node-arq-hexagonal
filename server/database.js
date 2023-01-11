302782cea420d09314af9ccc46b91c9a6a03b247
CristianX <thecristianx@hotmail.com>
2023-01-10 22:58:54 -0500

    Añadiendo implementación parcial de conección a la base de datos mongodb

---
+++ server/database.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+const logger = require('./config/logger');
+
+exports.connect = (
+  { protocol = 'mongodb', url, username = '', password = '' },
+  options = {}
+) => {
+  let dburl = '';
+}
+
+// Requiere autenticación
+if( username && password ) {
+  dburl = `${protocol}://${username}:${password}@${url}`;
+} else {
+  dburl = `${protocol}://${url}`;
+}
+
+// Conexión a la base de datos
+mongoose.connect(dburl, {
+  ...options,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on('open', () => {
+  logger.info('Conectado a la base de datos');
+});
+
+mongoose.connection.on('close', () => {
+  logger.info('Desconectado de la base de datos');
+});
+
+mongoose.connection.on('error', (err) => {
+  logger.error('Error en la conexión a la base de datos', err);
+});
+
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    logger.info('Conexión a la base de datos cerrada por el proceso');
+    process.exit(0);
+  });
+});
+
+exports.disconnect = () => {
+  mongoose.connection.close(() => {
+    logger.info('Conexión a la base de datos cerrada');
+  });
+};
