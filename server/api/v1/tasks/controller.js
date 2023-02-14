ade6ea5b4481a62f973f604cf86676ca74ebb393
CristianX <thecristianx@hotmail.com>
2023-02-13 21:56:38 -0500

    Procesando parámetro id, actualizar y borrar para Tasks

--- server/api/v1/tasks/controller.js
+++ server/api/v1/tasks/controller.js
@@ -1,5 +1,26 @@
 const Model = require('./model');
 
+exports.id = async (req, res, next, id) => {
+  try {
+    const doc = await Model.findById(id).exec();
+
+    if (!doc) {
+      const message = `${Model.modelName} not found`;
+
+      next({
+        message,
+        statusCode: 404,
+        level: 'warn',
+      });
+    } else {
+      req.doc = doc;
+      next();
+    }
+  } catch (err) {
+    next(new Error(err));
+  }
+};
+
 exports.create = async (req, res, next) => {
   const { body = {} } = req;
   const document = new Model(body);
@@ -11,7 +32,6 @@ exports.create = async (req, res, next) => {
   } catch (err) {
     next(new Error(err));
   }
-
 };
 
 exports.all = async (req, res, next) => {
@@ -23,26 +43,32 @@ exports.all = async (req, res, next) => {
   }
 };
 
-exports.read = (req, res, next) => {
-  const { params = {} } = req;
-  const { id } = params;
+exports.read = async (req, res, next) => {
+  const { doc = {} } = req;
 
-  res.json({ id });
+  res.json(doc);
 };
 
-exports.update = (req, res, next) => {
-  const {body = {}, params = {}} = req;
-  const { id } = params;
-  res.json({
-    id,
-    body,
-  });
+exports.update = async (req, res, next) => {
+  const { doc = {}, body = {} } = req;
+
+  Object.assign(doc, body);
+
+  try {
+    const updated = await doc.save();
+    res.json(updated);
+  } catch (err) {
+    next(new Error(err));
+  }
 };
 
-exports.delete = (req, res, next) => {
-  const { params = {} } = req;
-  const { id } = params;
-  res.json({
-    id,
-  });
+exports.delete = async (req, res, next) => {
+  const { doc = {} } = req;
+
+  try {
+    const removed = await doc.remove();
+    res.json(removed);
+  } catch (err) {
+    next(new Error(err));
+  }
 };
--- server/api/v1/tasks/routes.js
+++ server/api/v1/tasks/routes.js
@@ -11,6 +11,8 @@ const controller = require('./controller');
 
 router.route('/').post(controller.create).get(controller.all);
 
+router.param('id', controller.id);
+
 router
   .route('/:id')
   .get(controller.read)
